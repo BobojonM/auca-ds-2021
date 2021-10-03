7bc5c4b5c7cef035482ba809155b5f0bafbefcf1
Bobojon Abdulloev <abdulloev_b@auca.kg>
2021-10-03 21:17:07 +0200

    lab02 p01 	Wrong answer

--- lab02/p01/main.cpp
+++ lab02/p01/main.cpp
@@ -5,14 +5,42 @@ int sz(const C &c) { return static_cast<int>(c.size()); }
 
 using namespace std;
 
+int cycleLength(int x)
+{
+    int cnt = 0;
+    while (x != 1)
+    {
+        if (x % 2 == 1 && x != 1)
+        {
+            cnt++;
+            x = 3 * x + 1;
+        }
+        else
+        {
+            cnt++;
+            x = x / 2;
+        }
+    }
+    return cnt;
+}
+
 int main()
 {
     iostream::sync_with_stdio(false);
 
-    int a;
-    int b;
-    while (cin >> a >> b)
+    int i;
+    int j;
+    while (cin >> i >> j)
     {
-        cout << a << " " << b << " " << a + b << "\n";
+        int maxCycle = 0;
+        for (int x = i; x <= j; x++)
+        {
+            int cycle = cycleLength(x);
+            if (cycle > maxCycle)
+            {
+                maxCycle = cycle;
+            }
+        }
+        cout << i << " " << j << " " << maxCycle + 1 << "\n";
     }
 }
