c42fddda490c1412e7a45b8e179b4e709523a186
Bobojon Abdulloev <abdulloev_b@auca.kg>
2021-10-08 07:45:46 +0200

    created expirements folder lab02 p08 Wrong answer

---
+++ lab02/p08/main.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+template <typename C>
+int sz(const C &c) { return static_cast<int>(c.size()); }
+
+using namespace std;
+
+int main()
+{
+    iostream::sync_with_stdio(false);
+
+    string line;
+    int cnt = 0;
+    while (getline(cin, line))
+    {
+        int ln = line.length();
+        for (int i = 0; i < ln; i++)
+        {
+            char ch = line[i];
+            if (ch == '"')
+            {
+                if (cnt % 2 == 0)
+                {
+                    cout << "``";
+                }
+                else
+                {
+                    cout << "''";
+                }
+                cnt++;
+            }
+            else
+            {
+                cout << ch;
+            }
+        }
+    }
+}
