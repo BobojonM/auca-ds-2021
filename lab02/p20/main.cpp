8b5e2abcb93a2739189f8af6ffe2e430da9ef8cb
Bobojon Abdulloev <abdulloev_b@auca.kg>
2021-11-09 17:42:12 +0100

    lab02 p20 Wrong answer

---
+++ lab02/p20/main.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+template <typename C>
+int sz(const C &c) { return static_cast<int>(c.size()); }
+
+using namespace std;
+
+int main()
+{
+    iostream::sync_with_stdio(false);
+
+    int t, cnt = 0;
+    string name, gName;
+    while (cin >> t)
+    {
+        if (cnt)
+            cout << "\n";
+
+        map<string, int> db;
+        vector<string> names(t);
+
+        for (int i = 0; i < t; i++)
+            cin >> names[i];
+
+        int sum, g, a = 0;
+        for (int i = 0; i < t; i++)
+        {
+            cin >> name >> sum >> g;
+            db[name] -= sum;
+
+            if (g)
+                a = sum / g;
+            if (a)
+                db[name] += sum - (a * g);
+
+            for (int j = 0; j < g; j++)
+            {
+                cin >> gName;
+                db[gName] += a;
+            }
+        }
+
+        for (auto n : names)
+            cout << n << " " << db[n] << "\n";
+
+        cnt++;
+    }
+}
