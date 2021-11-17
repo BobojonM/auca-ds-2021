d361cc8114c27b9a94cada8a0ee84ca4ee82e487
Bobojon Abdulloev <abdulloev_b@auca.kg>
2021-11-17 14:37:25 +0100

    lab01 p06 completed

--- lab01/p06/main.cpp
+++ lab01/p06/main.cpp
@@ -108,6 +108,19 @@ TEST_CASE("vector`s move constructor")
     REQUIRE(v2[2] == 3);
 }
 
+TEST_CASE("vector's move assignment operator")
+{
+    vector<int> v = {1, 2, 3};
+
+    vector<int> v2 = {10, 20};
+
+    v2 = move(v);
+
+    REQUIRE(v2.size() == 3);
+    REQUIRE(containerToStr(v2) == "{1, 2, 3}");
+    REQUIRE(v.empty());
+}
+
 TEST_CASE("vector`s assignment operator")
 {
     vector<int> v = {1, 2, 3};
@@ -130,4 +143,116 @@ TEST_CASE("vector`s at operator")
     vector<int> v = {10, 20, 30};
 
     REQUIRE_THROWS_AS(v.at(3), out_of_range);
+}
+
+TEST_CASE("vector's methods front(), back()")
+{
+    vector<int> v = {4, 1, 2, 7, 6};
+
+    REQUIRE(v.front() == 4);
+
+    v.front() = 10;
+
+    REQUIRE(v.front() == 10);
+
+    REQUIRE(v.back() == 6);
+
+    v.back() = 11;
+
+    REQUIRE(v.back() == 11);
+}
+
+struct Student
+{
+    int mID;
+    int mBirthYear;
+    Student(int ID, int birthYear)
+        : mID(ID), mBirthYear(birthYear)
+    {
+    }
+
+    int getID() const
+    {
+        return mID;
+    }
+};
+
+TEST_CASE("vector< T>::iterator")
+{
+    vector<int> v = {10, 5, 2, 3};
+
+    // vector<int> ::iterator it = v.begin();
+
+    auto it = v.begin();
+
+    REQUIRE(*it == 10);
+
+    ++it;
+    REQUIRE(*it == 5);
+
+    REQUIRE(*++it == 2);
+    REQUIRE(*it++ == 2);
+    REQUIRE(*it == 3);
+
+    auto it2 = v.end();
+    REQUIRE(it != it2);
+    REQUIRE(it < it2);
+
+    --it2;
+    REQUIRE(*it2 == 3);
+
+    it2 -= 2;
+
+    REQUIRE(*it2 == 5);
+
+    REQUIRE(it2 - v.begin() == 1);
+
+    vector<Student> s = {{1001, 2003}, {1002, 2004}};
+
+    auto it3 = s.begin();
+
+    REQUIRE((*it3).mBirthYear == 2003);
+    REQUIRE(it3->mBirthYear == 2003);
+    REQUIRE(it3->getID() == 1001);
+}
+
+TEST_CASE("vector<T> erase() operator")
+{
+    vector<int> vc = {7, 20, 40, 80, 5};
+
+    auto it = vc.begin();
+    ++it;
+
+    vc.erase(it);
+
+    REQUIRE(vc.size() == 4);
+    REQUIRE(containerToStr(vc) == "{7, 40, 80, 5}");
+
+    auto it2 = vc.begin();
+
+    vc.erase(it2, it2 + 2);
+
+    REQUIRE(vc.size() == 2);
+    REQUIRE(containerToStr(vc) == "{80, 5}");
+}
+
+TEST_CASE("vector< T> insert() operator")
+{
+
+    vector<int> vc = {1, 2, 3};
+
+    auto it = vc.begin();
+    ++it;
+
+    vc.insert(it, 4);
+
+    REQUIRE(vc.size() == 4);
+    REQUIRE(containerToStr(vc) == "{1, 4, 2, 3}");
+
+    auto it2 = vc.begin();
+
+    vc.insert(it2 + 2, 3, 3);
+
+    REQUIRE(vc.size() == 7);
+    REQUIRE(containerToStr(vc) == "{1, 4, 3, 3, 3, 2, 3}");
 }
