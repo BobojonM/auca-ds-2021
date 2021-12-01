e089e44a106ed148103b10c6e51e0a97565f6216
Bobojon Abdulloev <abdulloev_b@auca.kg>
2021-12-01 20:20:25 +0100

    created string & int constructors, add operator >>

--- project01/BigInt/BigInt.hpp
+++ project01/BigInt/BigInt.hpp
@@ -2,6 +2,7 @@
 
 #include <vector>
 #include <iosfwd>
+#include <string>
 
 class BigInt
 {
@@ -15,6 +16,18 @@ public:
         mDigits.push_back(0);
     }
 
+    BigInt(const std::string &n)
+        : mIsNegative(false)
+    {
+        parseString(n);
+    }
+
+    BigInt(const long long &n)
+        : mIsNegative(false)
+    {
+        parseString(std::to_string(n));
+    }
+
     const std::vector<int> &digits() const
     {
         return mDigits;
@@ -24,7 +37,53 @@ public:
     {
         return mIsNegative;
     }
+
+    void parseString(const std::string &m)
+    {
+        if (m.empty())
+        {
+            throw std::runtime_error("Incorrect input ");
+        }
+
+        std::string n = m;
+        if (n[0] == '+' || n[0] == '-')
+        {
+            mIsNegative = (n[0] == '-' ? true : false);
+            n = n.substr(1);
+        }
+
+        for (char ch : n)
+        {
+            if (!isdigit(ch))
+            {
+                throw std::runtime_error("Incorrect input ");
+            }
+            else
+            {
+                mDigits.push_back(ch - '0');
+            }
+        }
+
+        for (int i = 0; mDigits[i] == 0;)
+        {
+            mDigits.erase(mDigits.begin() + i);
+        }
+    }
 };
+
+std::istream &operator>>(std::istream &sinp, BigInt &b)
+{
+    std::string str, line;
+
+    while (getline(sinp, line))
+    {
+        str += line;
+    }
+
+    b = BigInt(str);
+
+    return sinp;
+}
 
 std::ostream &operator<<(std::ostream &sout, const BigInt &b)
 {
--- project01/BigInt/main.cpp
+++ project01/BigInt/main.cpp
@@ -7,11 +7,118 @@
 
 using namespace std;
 
-TEST_CASE("Default constructor")
+TEST_CASE("BigInt: Default constructor")
 {
     BigInt a;
     ostringstream sout;
     sout << a;
 
     REQUIRE(sout.str() == "0");
+}
+
+TEST_CASE("BigInt: string constructor")
+{
+    ostringstream sout;
+
+    SUBCASE("Test with 111111111111111")
+    {
+        BigInt a("111111111111111");
+        sout << a;
+        REQUIRE(sout.str() == "111111111111111");
+    }
+
+    SUBCASE("Test with -111111111111111")
+    {
+        BigInt a("-111111111111111");
+        sout << a;
+        REQUIRE(sout.str() == "-111111111111111");
+    }
+
+    SUBCASE("Test with +123456789")
+    {
+        BigInt a("+123456789");
+        sout << a;
+        REQUIRE(sout.str() == "123456789");
+    }
+
+    SUBCASE("Test with 000011234")
+    {
+        BigInt a("000011234");
+        sout << a;
+        REQUIRE(sout.str() == "11234");
+    }
+}
+
+TEST_CASE("bigInt: int constructor")
+{
+    ostringstream sout;
+
+    SUBCASE("Test with -1000000000")
+    {
+        BigInt a(-1000000000);
+        sout << a;
+        REQUIRE(sout.str() == "-1000000000");
+    }
+
+    SUBCASE("Test with 100000000")
+    {
+        BigInt a(100000000);
+        sout << a;
+        REQUIRE(sout.str() == "100000000");
+    }
+
+    SUBCASE("Test with +123456789")
+    {
+        BigInt a(+123456789);
+        sout << a;
+        REQUIRE(sout.str() == "123456789");
+    }
+
+    SUBCASE("Test with 0123")
+    {
+        BigInt a(0123);
+        sout << a;
+        REQUIRE(sout.str() == "83");
+    }
+}
+
+TEST_CASE("bigInt: intput operator >>")
+{
+
+    ostringstream sout;
+    SUBCASE("Test with -1000000000")
+    {
+        istringstream sinp("-1000000000");
+        BigInt a;
+        sinp >> a;
+        sout << a;
+        REQUIRE(sout.str() == "-1000000000");
+    }
+
+    SUBCASE("Test with 100000000")
+    {
+        istringstream sinp("100000000");
+        BigInt a;
+        sinp >> a;
+        sout << a;
+        REQUIRE(sout.str() == "100000000");
+    }
+
+    SUBCASE("Test with +123456789")
+    {
+        istringstream sinp("+123456789");
+        BigInt a;
+        sinp >> a;
+        sout << a;
+        REQUIRE(sout.str() == "123456789");
+    }
+
+    SUBCASE("Test with 0123")
+    {
+        istringstream sinp("0123");
+        BigInt a;
+        sinp >> a;
+        sout << a;
+        REQUIRE(sout.str() == "123");
+    }
 }
