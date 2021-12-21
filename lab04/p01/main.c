191b4decbde102240b996f50d6d430a3b8251dfa
Bobojon Abdulloev <abdulloev_b@auca.kg>
2021-12-21 09:00:39 +0100

    lab04 p01 fixed

--- lab04/p01/main.c
+++ lab04/p01/main.c
@@ -1,25 +1,24 @@
 #include <stdio.h>
 
-int main(void)
+int main()
 {
     int x;
     int y;
-
     int *p;
 
     x = 1;
     p = &x;
 
-    printf("x = %d\n", x);
-    printf("address of x = %p\n", (void *)p);
-    printf("x = %d\n", *p); //derefence operator
+    printf("value of x: %d\n", x);
+    printf("adress of x: %p\n", p);
+    printf("value of x: %d\n", *p);
 
     *p = 2;
-    printf("x = %d\n", x);
-    printf("x = %d\n", *p);
+
+    printf("value of x: %d\n", x);
+    printf("value of x: %d\n", *p);
 
     y = *p * *p;
-    printf("y = %d\n", y);
-    printf("y = %ld\n", sizeof(int));
-    printf("y = %ld\n", sizeof(int *));
+
+    printf("value of y: %d\n", y);
 }
