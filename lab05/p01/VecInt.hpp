cd1a090407d289763ab43f19806be8a0d711da98
Bobojon Abdulloev <abdulloev_b@auca.kg>
2021-12-23 00:03:35 +0100

    created class VecInt

---
+++ lab05/p01/VecInt.hpp
@@ -0,0 +1,88 @@
+#ifndef VECINT_HPP
+#define VECINT_HPP
+
+#include <cstddef>
+#include <iostream>
+
+#ifdef AUCA_DEBUG
+#include <string>
+#endif
+
+class VecInt
+{
+    int *data;
+    size_t sz;
+    size_t cp;
+
+public:
+    using Iter = int *;
+    using CIter = const int *;
+
+    VecInt()
+        : data(nullptr), sz(0), cp(0)
+    {
+    }
+    explicit VecInt(int size, int initValue)
+        : data(new int[size]), sz(size), cp(size)
+    {
+        for (int i = 0; i < size; i++)
+            data[i] = initValue;
+    }
+
+    ~VecInt()
+    {
+        std::cout << "~VecInt: " << sz << " integers released\n";
+        delete[] data;
+    }
+
+    size_t size() const
+    {
+        return sz;
+    }
+
+    Iter begin()
+    {
+        return data;
+    }
+
+    Iter end()
+    {
+        return data + sz;
+    }
+
+    CIter begin() const
+    {
+        return data;
+    }
+
+    CIter end() const
+    {
+        return data + sz;
+    }
+
+    const int &operator[](std::size_t index) const
+    {
+#ifdef AUCA_DEBUG
+        if (sz <= index)
+        {
+            trow std::runtime_error("VecInt: incorrect index: " + std::to_string(index))
+        }
+#endif
+        return data[index];
+    }
+
+    int &operator[](std::size_t index)
+    {
+#ifdef AUCA_DEBUG
+        if (sz <= index)
+        {
+            trow std::runtime_error("VecInt: incorrect index: " + std::to_string(index))
+        }
+#endif
+        return data[index];
+    }
+
+    void push_back(int x);
+};
+
+#endif
---
+++ lab05/p01/main.cpp
@@ -0,0 +1,57 @@
+#include "../../au/algol.hpp"
+#include <iostream>
+#include "VecInt.hpp"
+using namespace std;
+
+void printVecInt(const VecInt &v)
+{
+    // for (int e : v)
+    // {
+    //     cout << e << " ";
+    // }
+    for (size_t i = 0; i < v.size(); ++i)
+    {
+        cout << v[i] << " ";
+    }
+    cout << "\n";
+}
+
+void p01()
+{
+    cout << "The size of array: ";
+    int n;
+    cin >> n;
+
+    VecInt v(n, 0);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+
+    printVecInt(v);
+    auReverse(v.begin(), v.end());
+    printVecInt(v);
+}
+
+void p02()
+{
+    // vector <int> v;
+    VecInt v;
+
+    for (int x; cin >> x;)
+    {
+        v.push_back(x);
+    }
+
+    printVecInt(v);
+    auReverse(v.begin(), v.end());
+    //  auReverse(v.data, v.data + v.sz); functions begin(), end() inlined
+    printVecInt(v);
+}
+
+int main()
+{
+    //p01();
+    p02();
+}
