215dffb8e25651c1a4538d31df26c30f73973c05
Bobojon Abdulloev <abdulloev_b@auca.kg>
2021-12-27 11:38:54 +0100

    Problem 465 Wrong answer

---
+++ project01/p465/main.cpp
@@ -0,0 +1,33 @@
+#include "../BigInt/BigInt.hpp"
+
+#include <climits>
+
+using namespace std;
+
+int main()
+{
+    string s1, cmd, s2;
+    BigInt mx(INT_MAX);
+
+    while (cin >> s1 >> cmd >> s2)
+    {
+        BigInt a(s1);
+        BigInt b(s2);
+
+        cout << s1 << " " << cmd << " " << s2 << "\n";
+
+        if (a >= mx)
+            cout << "first number too big\n";
+        if (b >= mx)
+            cout << "second number too big\n";
+
+        BigInt res;
+        if (cmd == "+")
+            res = (a + b);
+        else
+            res = (a * b);
+
+        if (res >= mx)
+            cout << "result too big\n";
+    }
+}
